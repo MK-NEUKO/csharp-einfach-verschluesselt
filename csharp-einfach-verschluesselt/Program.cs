92ba09310ec9bddc501d7557886b8e47c0dc996a
Michael Koch <m.koch@know-how-akademie.de>
2021-01-17 22:49:30 +0100

    Grundgerüst des Programms erstellt und ausgeführt.

--- csharp-einfach-verschluesselt/Program.cs
+++ csharp-einfach-verschluesselt/Program.cs
@@ -6,7 +6,37 @@ namespace csharp_einfach_verschluesselt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Text vom User erfragen.
+            string userInput = GetUserInput();
+
+            // Alle Buchstaben zu Großbuchstaben ändern, alle Umlaute normalisieren.
+            string userInputNormalized = NormalizeUserInput(userInput);
+
+            // Text Verschlüsseln
+            string outputOfRotate13 = Rotate13(userInputNormalized);
+
+            // verschlüsselten Text ausgeben
+            Console.WriteLine(outputOfRotate13);
+
+            Console.ReadKey();
         }
+
+        private static string GetUserInput()
+        {
+            Console.WriteLine("Text vom User holen!");
+            return "userInput";
+        }
+
+        private static string NormalizeUserInput(string userInput)
+        {
+            Console.WriteLine("Text normalisieren!");
+            return "Normalisierter Text";
+        }
+
+        private static string Rotate13(string userInputNormalized)
+        {
+            Console.WriteLine("Text verschlüsseln!");
+            return "verschlüsselter Text";
+        }             
     }
 }
